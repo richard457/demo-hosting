5db59685e0790d7f2bbfdb537bbbf99684dd2023
Ptrick Niyongira <niyongirapatrick29@gmail.com>
2017-06-14 12:30:46 +0200

    changing route file

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->get('/users', function (Request $request) {
+    //return $request->user();
+});
+
+Route::middleware('auth:api')->get('/dev', function (Request $request) {
+    //return $request->user();
 });
